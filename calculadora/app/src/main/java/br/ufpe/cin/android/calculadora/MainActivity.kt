b0aa7d0039bbcad253429b76abc0223f8dd7883c
Edjan Michiles <esvm@cin.ufpe.br>
2019-08-23 22:43:37 -0300

    feat(first class): add first class exercise code

--- calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -2,14 +2,50 @@ package br.ufpe.cin.android.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setButtonListeners()
     }
 
+    // seta o listener de todos os botões concatenando o texto do botão no que já tem na caixa de texto
+    // para os casos de números e operadores.
+    // no caso do clear, limpa o que tem na caixa de texto
+    // no caso do equal, calcula o valor e seta em text_info
+    private fun setButtonListeners() {
+        btn_0.setOnClickListener{setText(btn_0.text)}
+        btn_1.setOnClickListener{setText(btn_1.text)}
+        btn_2.setOnClickListener{setText(btn_2.text)}
+        btn_3.setOnClickListener{setText(btn_3.text)}
+        btn_4.setOnClickListener{setText(btn_4.text)}
+        btn_5.setOnClickListener{setText(btn_5.text)}
+        btn_6.setOnClickListener{setText(btn_6.text)}
+        btn_7.setOnClickListener{setText(btn_7.text)}
+        btn_8.setOnClickListener{setText(btn_8.text)}
+        btn_9.setOnClickListener{setText(btn_9.text)}
+        btn_Dot.setOnClickListener{setText(btn_Dot.text)}
+        btn_LParen.setOnClickListener{setText(btn_LParen.text)}
+        btn_RParen.setOnClickListener{setText(btn_RParen.text)}
+        btn_Power.setOnClickListener{setText(btn_Power.text)}
+        btn_Add.setOnClickListener{setText(btn_Add.text)}
+        btn_Subtract.setOnClickListener{setText(btn_Subtract.text)}
+        btn_Divide.setOnClickListener{setText(btn_Divide.text)}
+        btn_Multiply.setOnClickListener{setText(btn_Multiply.text)}
+        btn_Equal.setOnClickListener{text_info.text = eval(text_calc.text.toString()).toString()}
+        btn_Clear.setOnClickListener{text_calc.setText("")}
+    }
+
+    // concatena o texto do botão com o que já tem na caixa de texto
+    private fun setText(text: CharSequence?) {
+        text_calc.text = Editable.Factory.getInstance().newEditable(text_calc.text.toString() + text)
+    }
 
     //Como usar a função:
     // eval("2+2") == 4.0
@@ -37,7 +73,9 @@ class MainActivity : AppCompatActivity() {
             fun parse(): Double {
                 nextChar()
                 val x = parseExpression()
-                if (pos < str.length) throw RuntimeException("Caractere inesperado: " + ch)
+                // exibe o toast ao invés de estourar a exception. $ch é um template e indica que o valor da variável
+                // ch será inserida nesse local da string
+                if (pos < str.length) showToast("Caractere inesperado: $ch")
                 return x
             }
 
@@ -94,13 +132,21 @@ class MainActivity : AppCompatActivity() {
                     else if (func == "tan")
                         x = Math.tan(Math.toRadians(x))
                     else
-                        throw RuntimeException("Função desconhecida: " + func)
+                        showToast("Função desconhecida: $func")
                 } else {
-                    throw RuntimeException("Caractere inesperado: " + ch.toChar())
+                    // se der erro, exibe o toast e retorna zero
+                    showToast("Caractere inesperado: $ch")
+                    return 0.0
                 }
                 if (eat('^')) x = Math.pow(x, parseFactor()) // potência
                 return x
             }
         }.parse()
+    }
+
+    // exibe o toast com o erro na variável text. O Toast.LENGTH_LONG indica por quanto tempo
+    // o toast vai permanecer na tela
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
     }
 }
